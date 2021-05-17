f6f3acd99e84c3c79cd97949d1b92f38c91d1afb
VitorTheGreat <vitor.services@gmail.com>
2021-05-17 20:43:23 -0300

    finished basice crud functions with sequelize

--- src/controllers/book.ts
+++ src/controllers/book.ts
@@ -37,7 +37,12 @@ const getById = (req: Request, res: Response, next: NextFunction) => {
   const id = req.params.id;
 
   models.Book.findByPk(id)
-    .then((result: any) => res.status(200).json(result))
+    .then((result: any) => {
+      if(!result) {
+        res.status(404).json({message: "Book not found"})
+      }
+      res.status(200).json(result)
+    })
     .catch((err: any) => {
       logging.error(NAMESPACE, err.message, err)
 
@@ -52,6 +57,59 @@ const getById = (req: Request, res: Response, next: NextFunction) => {
 const getAllBooks = (req: Request, res: Response, next: NextFunction) => {
   logging.info(NAMESPACE, `Getting all Books`);
 
+  models.Book.findAll()
+    .then((results: any) => res.status(200).json({ results }))
+    .catch((err: any) => {
+      logging.error(NAMESPACE, err.message, err)
+
+      return res.status(500).json({
+        message: "ERROR: ID not founded",
+        error: err
+      })
+    })
+
 };
 
-export default { getAllBooks, createBook, getById };
+const updateBook = (req: Request, res: Response, next: NextFunction) => {
+  logging.info(NAMESPACE, `Updating a book`);
+
+  const id = req.params.id;
+
+  let { author, title } = req.body
+
+  const updatedInfo = {
+    author,
+    title
+  };
+
+  models.Book.update(updatedInfo, { where: { id } })
+    .then((result: any) => res.status(200).json({ result })) //? the result here, returns a boolean value!
+    .catch((err: any) => {
+      logging.error(NAMESPACE, err.message, err)
+
+      return res.status(500).json({
+        message: "ERROR: Book not updated",
+        error: err
+      })
+    })
+
+}
+
+const deleteBook = (req: Request, res: Response, next: NextFunction) => {
+  logging.info(NAMESPACE, `Deleting a book`);
+
+  const id = req.params.id;
+
+  models.Book.destroy({ where: { id } })
+    .then((result: any) => res.status(200).json({ result })) //? the result here, returns a boolean value!
+    .catch((err: any) => {
+      logging.error(NAMESPACE, err.message, err)
+
+      return res.status(500).json({
+        message: "ERROR: Book not deleted",
+        error: err
+      })
+    })
+}
+
+export default { getAllBooks, createBook, getById, updateBook, deleteBook };
--- src/routes/book.ts
+++ src/routes/book.ts
@@ -4,6 +4,9 @@ import controller from "../controllers/book";
 const router = express.Router();
 
 router.get("/get/books", controller.getAllBooks);
+router.get("/get/books/:id", controller.getById);
 router.post("/post/books", controller.createBook);
+router.patch("/update/books/:id", controller.updateBook)
+router.delete("/delete/books/:id", controller.deleteBook)
 
 export = router;
